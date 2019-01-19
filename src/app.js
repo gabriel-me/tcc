22990200df69a86c50dbe65600a58c343eb42f1b
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2019-01-18 23:47:16 -0200

    Primeiro commit

---
+++ src/app.js
@@ -0,0 +1,24 @@
+// Modules
+const express = require('express');
+const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
+const start = require('./routes/start');
+const app = express();
+
+// Config
+mongoose.Promise = global.Promise;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname, 'assets')))
+
+// Routes
+app.use('/', start);
+
+// Server
+app.listen('8081', () => {
+    console.log('rum');
+});
