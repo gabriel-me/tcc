923f15355209b8d873079519b128dcddaad6f8fd
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2019-03-07 19:36:02 -0300

    Menu finalizado

--- src/public/javascripts/main.js
+++ src/public/javascripts/main.js
@@ -1,10 +1,21 @@
 let $menu = document.getElementById('menu');
-let $arrowButton = document.getElementById('arrowButton');
+let $footer = document.querySelector('#menu footer');
+let $menuButton = document.getElementById('menuButton');
 
-$arrowButton.addEventListener('click', () => {
+$menuButton.addEventListener('click', () => {
     if(window.innerWidth > 600) {
-        ($menu.style.marginLeft != '-17%') ? $menu.style.marginLeft = '-17%' : $menu.style.marginLeft = '0%';
+        if ($menu.style.marginLeft != '-17%') {
+            $menu.style.marginLeft = '-17%';
+            $footer.style.borderColor = 'white';
+        } else {
+            $menu.style.marginLeft = '0%';
+            $footer.style.borderColor = '#E6E6E6';
+        }
     } else {
-        ($menu.style.marginLeft != '-100%') ? $menu.style.marginLeft = '-100%' : $menu.style.marginLeft = '0%';
+        if ($menu.style.marginLeft != '-100%') {
+            $menu.style.marginLeft = '-100%';
+        } else {
+            $menu.style.marginLeft = '0%';
+        }
     }
 });
