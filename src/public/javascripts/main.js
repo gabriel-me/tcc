eed34802e533674e501ae1a821fb926401d6142e
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2019-03-10 20:16:35 -0300

    Menu modificado(tema dark)

--- src/public/javascripts/main.js
+++ src/public/javascripts/main.js
@@ -1,15 +1,18 @@
 let $menu = document.getElementById('menu');
+let $navbar = document.querySelector('#menu nav');
 let $footer = document.querySelector('#menu footer');
 let $menuButton = document.getElementById('menuButton');
 
 $menuButton.addEventListener('click', () => {
-    if(window.innerWidth > 600) {
-        if ($menu.style.marginLeft != '-17%') {
-            $menu.style.marginLeft = '-17%';
-            $footer.style.borderColor = 'white';
+    if(window.innerWidth > 800) {
+        if ($menu.style.marginLeft != '-15rem') {
+            $menu.style.marginLeft = '-15rem';
+            $menu.style.backgroundColor = 'white';
+            $navbar.style.display = 'none';
         } else {
-            $menu.style.marginLeft = '0%';
-            $footer.style.borderColor = '#E6E6E6';
+            $menu.style.marginLeft = '0rem';
+            $menu.style.backgroundColor = '#070719';
+            $navbar.style.display = 'block';
         }
     } else {
         if ($menu.style.marginLeft != '-100%') {
@@ -17,5 +20,7 @@ $menuButton.addEventListener('click', () => {
         } else {
             $menu.style.marginLeft = '0%';
         }
+        $menu.style.backgroundColor = '#070719';
+        $navbar.style.display = 'block';
     }
 });
