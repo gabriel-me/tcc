f6c5f4e633cb252b2594058e3e7b2550f47e49ae
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2019-04-07 14:28:42 -0300

    Corrigido responsividade do menu

--- src/public/javascripts/main.js
+++ src/public/javascripts/main.js
@@ -2,16 +2,22 @@ let $menu = document.getElementById('menu');
 let $navbar = document.querySelector('#menu nav');
 let $footer = document.querySelector('#menu footer');
 let $menuButton = document.getElementById('menuButton');
+let $menuHeader = $menu.children[0];
+let $menuFooter = $menu.children[2];
 
 $menuButton.addEventListener('click', () => {
     if(window.innerWidth > 800) {
-        if ($menu.style.marginLeft != '-15rem') {
-            $menu.style.marginLeft = '-15rem';
+        if ($menu.style.marginLeft != '-14rem') {
+            $menu.style.marginLeft = '-14rem';
             $menu.style.backgroundColor = 'white';
+            $menuHeader.style.backgroundColor = 'white';
+            $menuFooter.style.backgroundColor = 'white';
             $navbar.style.display = 'none';
         } else {
             $menu.style.marginLeft = '0rem';
             $menu.style.backgroundColor = '#070719';
+            $menuHeader.style.backgroundColor = '#070719';
+            $menuFooter.style.backgroundColor = '#070719';
             $navbar.style.display = 'block';
         }
     } else {
@@ -28,12 +34,14 @@ $menuButton.addEventListener('click', () => {
 document.body.onresize = () => {
     if (window.innerWidth > 800) {
         if ($menu.style.marginLeft == '-100%') {
-            $menu.style.marginLeft = '-15rem';
+            $menu.style.marginLeft = '-14rem';
             $menu.style.backgroundColor = 'white';
+            $menuHeader.style.backgroundColor = 'white';
+            $menuFooter.style.backgroundColor = 'white';
             $navbar.style.display = 'none';
         }
     } else {
-        if ($menu.style.marginLeft == '-15rem') {
+        if ($menu.style.marginLeft == '-14rem') {
             $menu.style.marginLeft = '-100%';
         }
     }
