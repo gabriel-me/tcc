fb91f9c9c09481ddb7667d3cb7dc623b06900485
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2019-04-08 16:16:04 -0300

    Adicionado página inicial

--- src/routes/start.js
+++ src/routes/start.js
@@ -2,7 +2,7 @@ const express = require('express');
 const route = express.Router();
 
 route.get('/', (req, res) => {
-    res.render('start/site');
+    res.render('start/home');
 });
 
 route.get('/login', (req, res) => {
