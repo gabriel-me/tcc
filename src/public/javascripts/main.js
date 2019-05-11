62347ccbf1b36a03760905cd0af664c56a4935b2
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2019-05-11 16:45:44 -0300

    Menu finalizado

--- src/public/javascripts/main.js
+++ src/public/javascripts/main.js
@@ -1,38 +1,3 @@
-function menuControl() {
-  let $menuButtonRight = document.getElementById('menuButtonRight');
-  let $main = document.querySelector('main');
-  let $menu = document.getElementById('menu');
-  let $menuDiv = document.querySelector('#menu div');
-  let $menuNav = document.querySelector('#menu nav');
-  let $menuLogo = document.getElementById('menuLogo');
-  let $menuNavUl = document.querySelector('#menu nav ul');
-
-  if ($menuNav.style.display != 'none') {
-    $menuNav.style.display = 'none';
-    $menuLogo.style.display = 'none';
-    $menuButtonRight.style.display = 'inline-block';
-    if (window.innerWidth > 800) {
-      $menu.style.marginLeft = '-16rem';
-      $menuDiv.style.left = '16rem';
-      $main.style.marginLeft = '4rem';
-    } else {
-      $menu.style.marginLeft = '-100%';
-      $menuDiv.style.left = '100%';
-    }
-  } else {
-    $menu.style.marginLeft = '0';
-    $menuDiv.style.left = '0';
-    $menuNav.style.display = 'inline-block';
-    $menuLogo.style.display = 'flex';
-    $menuButtonRight.style.display = 'none';
-    if (window.innerWidth > 800) {
-      $main.style.marginLeft = '20rem';
-    } else {
-      $menuNavUl.style.marginLeft = '4rem';
-    }
-  }
-}
-
 function changeElementStyle(references) {
   references.forEach(reference => {
     let elements = document.querySelectorAll(reference.element);
@@ -41,5 +6,3 @@ function changeElementStyle(references) {
     });
   });
 }
-
-listenClickMenuButton();
