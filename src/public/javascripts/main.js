727f8e0d9e00fc33ea875655eb65734d50c8eec7
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2019-05-12 11:25:22 -0300

    Adicionado botão dropdown

--- src/public/javascripts/main.js
+++ src/public/javascripts/main.js
@@ -6,3 +6,19 @@ function changeElementStyle(references) {
     });
   });
 }
+
+function listenDropdownButton() {
+  let $dropdowns = document.querySelectorAll('.dropdown');
+  $dropdowns.forEach($dropdown => {
+    $dropdown.addEventListener('click', () => {
+      $dropdownMenu = $dropdown.children[1];
+      $dropdownMenu.style.display = 'flex';
+    });
+    $dropdown.addEventListener('focusout', () => {
+      $dropdownMenu = $dropdown.children[1];
+      $dropdownMenu.style.display = 'none';
+    });
+  });
+}
+
+listenDropdownButton();
