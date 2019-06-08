bb53eb7fbc7e31784553a67ec356ab559e581607
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2019-06-08 13:54:31 -0300

    CSS p/ projetos adicionado

--- src/routes/project.js
+++ src/routes/project.js
@@ -2,7 +2,9 @@ const express = require('express');
 const route = express.Router();
 
 route.get('/', (req, res) => {
-  res.render('project/projects');
+  res.render('project/projects', {
+    style: 'project.css'
+  });
 });
 
 module.exports = route;
