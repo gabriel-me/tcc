394571a6cbac94b186359b2917d4d09eaa9878a8
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2019-06-26 19:49:30 -0300

    Bug do componente list corrigido

--- src/components/utils/list/Row.jsx
+++ src/components/utils/list/Row.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './row.css'
 
 const renderColumns = data => 
-  data.map((d, i) => <div key={i} className={d.size || '_20'}>{d.text || ''}</div>);
+  data.map((d, i) => <div key={i} className={d.size + ' item' || '_20' + ' item'}>{d.text || ''}</div>);
 
 export default props => 
   <div className={`col ${props.position || ''}`}>{renderColumns(props.cols)}</div>
