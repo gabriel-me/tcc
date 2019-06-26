aba79807383194f6cdc5f121a85b7c6c0034d612
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2019-06-26 19:39:34 -0300

    Ajustado CSS do componente Icon

--- src/components/utils/icon/Icon.jsx
+++ src/components/utils/icon/Icon.jsx
@@ -2,15 +2,18 @@ import React from 'react'
 import './icon.css'
 
 export default props => 
-  <span
-    aria-label={props.title || ''}
-    className={`hint--${props.titleLocation} icon`}
-  >
-    <i
-      data-eva={props.icon || 'plus-circle-outline'}
-      data-eva-animation={props.animation || 'none'}
-      data-eva-fill={props.color || '#000000'}
-      data-eva-width={props.size || '1.6rem'}
-      data-eva-height={props.size || '1.6rem'}>
-    </i>
-  </span>
+  <div className="icon">
+    <span
+      aria-label={props.title || ''}
+      className={`hint--${props.titleLocation}`}
+    >
+      <i
+        data-eva={props.icon || 'plus-circle-outline'}
+        data-eva-animation={props.animation || 'none'}
+        data-eva-fill={props.color || '#000000'}
+        data-eva-width={props.size || '1.6rem'}
+        data-eva-height={props.size || '1.6rem'}>
+      </i>
+    </span>
+    {props.text ? <text>{props.text}</text> : ''}
+  </div>
