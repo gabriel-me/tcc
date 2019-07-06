3b0f56d35c67db37afcde21e2754f959f01845e6
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2019-07-06 18:05:16 -0300

    Consumindo rota de projetos e contatos

--- src/components/utils/menu/Menu.jsx
+++ src/components/utils/menu/Menu.jsx
@@ -1,48 +1,89 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Icon from '../icon/Icon'
 import Profile from '../profile/Profile'
 import Status from '../status/Status'
 import AddProject from '../../project/AddProject'
+import axios from 'axios'
 import './menu.css'
 
-export default () =>
-  <div className="menu">
-    <header>
+const URL = `http://localhost:8082/user/${window.localStorage.getItem('id')}`
+
+export default class Menu extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { 
+      listProject: [], 
+      listMessages: []
+    }
+  }
+
+  renderRows() {
+    let tasks = []
+    let projects = []
+    let messages = []
+
+    axios.get(URL).then(result => {
+      result.data.projects.forEach(project => {
+        projects.push({ id: project._id, name: project.name })
+      })
+      result.data.tasks.forEach(task => {
+        tasks.push({ id: task._id, name: task.name })
+      })
+      result.data.messages.forEach(message => {
+        messages.push({ id: message._id, sender: message.sender, status: message.status })
+      })
+
+      this.setState({
+        ...this.state,
+        listProject: projects,
+        listMessages: messages
+      })
+    })
+
+    return (
       <div>
-        <Profile title="Perfil" titleLocation="right" />
-        <Status status="online" />
+        <section>
+          <header>
+            <h4>Contatos</h4>
+            <Icon color="#BDBDBD" title="Nova conversa" titleLocation="left" />
+          </header>
+          <ul>
+            {this.state.listMessages.map(m => <li key={m.id}><Status status={m.status} />{m.sender}</li>)}
+          </ul>
+        </section>
+        <section>
+          <header>
+            <h4>Projetos</h4>
+            <AddProject />
+          </header>
+          <ul>
+            {this.state.listProject.map(p => <li key={p.id}>- {p.name}</li>)}
+          </ul>
+        </section>
       </div>
-      <Icon icon="arrowhead-left-outline" color="#BDBDBD" size="2rem" />
-    </header>
-    <section className="section-main">
-      <ul>
-        <a href="/home"><li><Icon icon="home-outline" color="#BDBDBD" />Página inicial</li></a>
-        <a href="/task"><li><Icon icon="checkmark-circle-2-outline" color="#BDBDBD" />Minhas tarefas</li></a>
-        <a href="/task"><li><Icon icon="bell-outline" color="#BDBDBD" />Caixa de entrada</li></a>
-        <a href="/task"><li><Icon icon="people-outline" color="#BDBDBD" />Usuários</li></a>
-      </ul>        
-    </section>
-    <section>
-      <header>
-        <h4>Conversas</h4>
-        <Icon color="#BDBDBD" title="Nova conversa" titleLocation="left" />
-      </header>
-      <ul>
-        <a href="/task"><li><Status />Eddie Vedder</li></a>
-        <a href="/task"><li><Status />Gabriel Marques de Souza</li></a>
-        <a href="/task"><li><Status status="online" />Roger Waters</li></a>
-      </ul>
-    </section>
-    <section>
-      <header>
-        <h4>Projetos</h4>
-        <AddProject />    
-      </header>
-      <ul>
-        <a href="/task"><li>- Novo Aplicativo de Vendas</li></a>
-        <a href="/task"><li>- Design do site principal</li></a>
-        <a href="/task"><li>- API REST, do sistema interno</li></a>
-        <a href="/task"><li>- TCC - FEMA 2019</li></a>
-      </ul>
-    </section>
-  </div>
+    )
+  }
+
+  render() {
+    return(
+      <div className="menu">
+        <header>
+          <div>
+            <Profile title="Perfil" titleLocation="right" />
+            <Status status="online" />
+          </div>
+          <Icon icon="arrowhead-left-outline" color="#BDBDBD" size="2rem" />
+        </header>
+        <section className="section-main">
+          <ul>
+            <a href="/home"><li><Icon icon="home-outline" color="#BDBDBD" />Página inicial</li></a>
+            <a href="/task"><li><Icon icon="checkmark-circle-2-outline" color="#BDBDBD" />Minhas tarefas</li></a>
+            <a href="/task"><li><Icon icon="bell-outline" color="#BDBDBD" />Caixa de entrada</li></a>
+            <a href="/task"><li><Icon icon="people-outline" color="#BDBDBD" />Usuários</li></a>
+          </ul>
+        </section>
+        {this.renderRows()}
+      </div>
+    )
+  }
+}
