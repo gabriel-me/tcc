4606fcff3b937df82efc86f0ff1eb51d629c30fc
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2019-07-08 20:33:32 -0300

    Adicionad ajax para pegar projeto

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,15 +1,40 @@
 import React, { Component } from 'react'
 import Header from '../utils/header/Header'
 import Menu from '../utils/menu/Menu'
+import axios from 'axios'
+
+let currentURL = ''
 
 export default class Project extends Component {
+  constructor(props) {
+    super(props)
+    currentURL = this.props.location.pathname
+    this.state = { project: '' }
+    this.getInfoProject = this.getInfoProject.bind(this)
+    this.getInfoProject()
+  }
+
+  getInfoProject() {
+    axios.get(`http://localhost:8082${currentURL}`).then(result => {
+      this.setState({
+        ...this.state,
+        project: result.data
+      })
+    })
+  }
+
   render() {
     return (
       <div className="content">
         <Menu />
         <Header title="Página inicial" />
         <main>
-          <h1>PROJECT</h1>
+          <section>
+            <h4>Members</h4>
+          </section>
+          <section>
+            <h4>Tasks</h4>
+          </section>
         </main>
       </div>
     )
