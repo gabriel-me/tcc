35ddb0a087b68a5f4b7cf9edbc933e54f897c5f3
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2019-07-09 10:05:41 -0300

    Adicionad rota para adicionar projeto

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -4,15 +4,17 @@ import Home from '../components/home/Home'
 import Task from '../components/task/Task'
 import Login from '../components/login/Template'
 import Project from '../components/project/Project'
+import AddProject from '../components/project/AddProject'
 
 export default () => (
   <BrowserRouter>
     <Switch name="http://localhost:3000">
-      <Route path="/" exact={true} component={Login} />
+      <Route path="/" exact component={Login} />
       <Route path="/signin" component={Login} />
       <Route path="/signup" component={Login} />
       <Route path="/home" component={Home} />
       <Route path="/task" component={Task} />
+      <Route path="/project/add" exact component={AddProject} />
       <Route path="/project/:id" component={Project} />
     </ Switch>
   </ BrowserRouter>
