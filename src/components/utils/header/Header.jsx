289557c1acd4d01b55f2ec93954eeac43022f50b
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2019-07-09 09:59:47 -0300

    Adicionado botão dropdown do header

--- src/components/utils/header/Header.jsx
+++ src/components/utils/header/Header.jsx
@@ -1,13 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Outline as Button } from '../button/Button'
 import { Search } from '../input/Input'
+import { Link } from 'react-router-dom'
 import './header.css'
 
-export default props => 
-  <header className="header">
-    <h1>{props.title || ''}</h1>
-    <div>
-      <Button label="Adicionar novo..." />
-      <Search label="Buscar projeto, tarefa ou usuário" option={props.option || ''} />
-    </div>
-  </header>
+export default class Header extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { ulButton: 'none' }
+    this.changeDisplayButton = this.changeDisplayButton.bind(this)
+  }
+
+  changeDisplayButton() {
+    this.setState({
+      ...this.state,
+      ulButton: this.state.ulButton === 'none' ? 'flex' : 'none'
+    })
+  }
+
+  render() {
+    return(
+      <header className="header">
+        <h1>{this.props.title || ''}</h1>
+        <div className="right-content">
+          <div className="add-container">
+            <Button label="Adicionar novo..." click={this.changeDisplayButton} />
+            <div className="dropdown">
+              <Link to="/"><span className={`${this.state.ulButton}`}>Tarefa</span></Link>
+              <Link to="/"><span className={`${this.state.ulButton}`}>Projeto</span></Link>
+              <Link to="/"><span className={`${this.state.ulButton}`}>Usuário</span></Link>
+            </div>
+          </div>
+          <Search label="Buscar projeto, tarefa ou usuário" option={this.props.option || ''} />
+        </div>
+      </header>
+    )
+  }
+}
