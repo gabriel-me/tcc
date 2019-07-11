451e2ac62b09e4bd5030e292a51742fc90429742
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2019-07-11 18:36:38 -0300

    Adicionado listagem de tarefas

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from '../components/home/Home'
 import Task from '../components/task/Task'
 import AddTask from '../components/task/AddTask'
+import ListTasks from '../components/task/ListTasks'
 import Login from '../components/login/Template'
 import Project from '../components/project/Project'
 import AddProject from '../components/project/AddProject'
@@ -14,6 +15,7 @@ export default () => (
       <Route path="/signin" component={Login} />
       <Route path="/signup" component={Login} />
       <Route path="/home" component={Home} />
+      <Route path="/task" exact component={ListTasks} />
       <Route path="/task/add" exact component={AddTask} />
       <Route path="/task/:id" component={Task} />
       <Route path="/project/add" exact component={AddProject} />
---
+++ src/components/task/ListTasks.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import axios from 'axios'
+import Header from '../utils/header/Header'
+import Menu from '../utils/menu/Menu'
+import Row from '../utils/list/Row'
+import ProgressBar from '../utils/chart/ProgressBar'
+import Tag from '../utils/tag/Tag'
+
+const URL = `http://localhost:8082/user/${window.localStorage.getItem('id')}`
+
+export default class Tasks extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { tasks: [] }
+    this.renderTasks = this.renderTasks.bind(this)
+    this.dateFormat = this.dateFormat.bind(this)
+    this.renderTasks()
+  }
+
+  dateFormat(date) {
+    date = new Date(date)
+    const day = date.getDate().toString().padStart('2', '0')
+    const month = date.getMonth().toString().padStart('2', '0')
+    return `${day}/${month}`
+  }
+
+  renderTasks() {
+    axios.get(URL).then(result => {
+      const tasks = result.data.tasks.map(task =>
+        <Row key={task._id} cols={[
+          { text: task.name, size: '_4' },
+          { text: task.sender.name, size: '_2' },
+          { text: task.project.name, size: '_2' },
+          { text: <ProgressBar size="60%" text={this.dateFormat(task.deadline)} />, size: '_2' }
+        ]} />
+      )
+      this.setState({
+        ...this.state,
+        tasks: tasks
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div className="content">
+        <Menu />
+        <Header title="Minhas tarefas" />
+        <main>
+          <Tag text="Example" />
+          <Tag text="Example" />
+          <Tag text="Example" />
+          <Tag text="Example" />
+          <Row position="first" cols={[
+            { text: 'Tarefa', size: '_4' },
+            { text: 'Remetente', size: '_2' },
+            { text: 'Projeto', size: '_2' },
+            { text: 'Prazo', size: '_2' }
+          ]} />
+          {this.state.tasks}
+        </main>
+      </div>
+    )
+  }
+}
