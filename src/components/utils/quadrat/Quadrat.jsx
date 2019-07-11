17e8a3db5841cc03f34648511a6e9864df40908e
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2019-07-11 12:09:23 -0300

    Alterado ícone folder para layer

--- src/components/utils/quadrat/Quadrat.jsx
+++ src/components/utils/quadrat/Quadrat.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Folder, Add } from '../icons/Icon'
+import { Layer, Add } from '../icons/Icon'
 import './quadrat.css'
 
 export default props =>
   <div className={`blue-quadrat ${props.type || ''}`}>
-    {props.type === 'white-quadrat' ? <Add /> : <Folder color="white" />} 
+    {props.type === 'white-quadrat' ? <Add /> : <Layer color="white" />} 
     {props.type === 'white-quadrat' ? '' : props.date}
     {props.text ? <p>{props.text}</p> : ''}
   </div>
