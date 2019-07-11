de5e9fcbdb9223d3a1da2ec2be4891b52dc96357
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2019-07-11 18:53:15 -0300

    Adicionado estado no menu

--- src/components/utils/menu/Menu.jsx
+++ src/components/utils/menu/Menu.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
 import Profile from '../profile/Profile'
 import Status from '../status/Status'
 import Projects from '../../project/ListProject'
@@ -7,35 +8,60 @@ import { Link } from 'react-router-dom'
 import Add, { Done, Home, Person, Notication, ArrowBack } from '../icons/Icon'
 import './menu.css'
 
-export default () =>
-  <div className="menu">
-    <header>
-      <div>
-        <Profile title="Perfil" titleLocation="right" />
-        <Status status="online" />
+const URL = `http://localhost:8082/user/${window.localStorage.getItem('id')}`
+
+export default class Menu extends Component {
+  constructor() {
+    super()
+    this.state = { user: '' }
+    this.getCurrentUser = this.getCurrentUser.bind(this)
+    this.getCurrentUser(this)
+  }
+
+  getCurrentUser() {
+    axios.get(URL).then(result => {
+      let user = result.data
+      this.setState({
+        ...this.state,
+        user: user
+      })
+    })
+  }
+
+  render() {
+    return(
+      <div className="menu">
+        <header>
+          <div>
+            <Profile title="Visualizar perfil" titleLocation="right" />
+            <Status status="online" />
+            <span className="name">{this.state.user.name}</span>
+          </div>
+          <ArrowBack color="#BDBDBD" />
+        </header>
+        <section className="section-main">
+          <ul>
+            <Link to="/home"><li><Home color="#BDBDBD" /> Página inicial</li></Link>
+            <Link to="/task"><li><Done color="#BDBDBD" /> Minhas tarefas</li></Link>
+            <Link to="/notification"><li><Notication color="#BDBDBD" /> Caixa de entrada</li></Link>
+            <Link to="/user"><li><Person color="#BDBDBD" /> Usuários</li></Link>
+          </ul>
+        </section>
+        <section>
+          <header>
+            <h4>Contatos</h4>
+            <Link to="/user/add"><Add color="#BDBDBD" /></Link>
+          </header>
+          <Messages />
+        </section>
+        <section>
+          <header>
+            <h4>Projetos</h4>
+            <Link to="/project/add"><Add color="#BDBDBD" /></Link>
+          </header>
+          <Projects />
+        </section>
       </div>
-      <ArrowBack color="#BDBDBD" />
-    </header>
-    <section className="section-main">
-      <ul>
-        <Link to="/home"><li><Home color="#BDBDBD" /> Página inicial</li></Link>
-        <Link to="/task"><li><Done color="#BDBDBD" /> Minhas tarefas</li></Link>
-        <Link to="/notification"><li><Notication color="#BDBDBD" /> Caixa de entrada</li></Link>
-        <Link to="/user"><li><Person color="#BDBDBD" /> Usuários</li></Link>
-      </ul>
-    </section>
-    <section>
-      <header>
-        <h4>Contatos</h4>
-        <Link to="/user/add"><Add color="#BDBDBD" /></Link>
-      </header>
-      <Messages />
-    </section>
-    <section>
-      <header>
-        <h4>Projetos</h4>
-        <Link to="/project/add"><Add color="#BDBDBD" /></Link>
-      </header>
-      <Projects />
-    </section>
-  </div>
+    )
+  }
+}
