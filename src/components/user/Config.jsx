c53b65bd0c06814150e54e06fb5c4bf185e0a708
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2019-07-26 23:18:08 -0300

    Retirado component do Config.jsx

--- src/components/user/Config.jsx
+++ src/components/user/Config.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Switch } from '../utils/input/Input'
 import { Link } from 'react-router-dom'
 import { Close } from '../utils/icons/Icon'
