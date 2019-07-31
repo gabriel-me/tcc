c88a3af238e1c751517ff63fbd1ff51f4ad346a1
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2019-07-30 22:12:32 -0300

    Aprimorado função data

--- src/components/utils/Date.js
+++ src/components/utils/Date.js
@@ -7,14 +7,15 @@ export default date => {
   return ''
 }
 
-export const brazilFormat = date => {
+export const brazilFormat = (date, fullDate = 'none') => {
   if (date) {
     date = new Date(date)
     let day = date.getDate() + 1
     let month = date.getMonth() + 1
+    let year = date.getFullYear()
     day = day.toString().padStart('2', '0')
     month = month.toString().padStart('2', '0')
-    return `${day}/${month}`
+    return fullDate === 'none' ? `${day}/${month}` : `${day}/${month}/${year}`
   }
   return ''
 }
