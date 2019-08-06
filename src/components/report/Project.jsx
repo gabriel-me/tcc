4ce4c421422f4045b2cad3ba3dfb41c2db9e5c9a
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2019-08-06 13:24:36 -0300

    Adicionado relatório de projetos

--- src/components/report/Project.jsx
+++ src/components/report/Project.jsx
@@ -1,15 +1,53 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
 import { Link } from 'react-router-dom'
 import { Close } from '../utils/icons/Icon'
+import Chart from '../utils/chart/Chart'
 
-export default () =>
-  <div>
-    <Link to="/report"><span id="close"><Close /></span></Link>
-    <div className="form-container">
-      <div className="form-content">
-        <header>
-          <h1>Projetos</h1>
-        </header>
+const URL = `http://localhost:8082/user/${window.localStorage.getItem('id')}`
+
+export default class extends Component {
+  constructor(props) {
+    super(props)
+    this.getChartData = this.getChartData.bind(this)
+    this.state = { chart: '' }
+    this.getChartData()
+  }
+
+  getChartData() {
+    axios.get(URL).then(result => {
+      let chartData = {
+        labels: ['Fora do prazo', 'Dentro do prazo'],
+        datasets: [
+          {
+            data: [result.data.doneProject, result.data.notDoneProject],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.6)',
+              'rgba(54, 162, 235, 0.6)'
+            ]
+          }
+        ]
+      }
+      this.setState({
+        ...this.state,
+        chart: <Chart chartData={chartData} legendPosition="bottom" />
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <Link to="/report"><span id="close"><Close /></span></Link>
+        <div className="form-container">
+          <div className="form-content">
+            <header>
+              <h1>Entrega de projetos</h1>
+            </header>
+            {this.state.chart}
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
+    )
+  }
+}
