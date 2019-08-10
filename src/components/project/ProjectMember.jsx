3481eef0e210827d9d2ea229cf354e402d15eb28
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2019-08-10 20:31:52 -0300

    Retirado usuário da listagem de integrantes

--- src/components/project/ProjectMember.jsx
+++ src/components/project/ProjectMember.jsx
@@ -48,6 +48,7 @@ export default class extends Component {
   }
 
   renderMembers(members) {
+    members = members.filter(member => member.id !== window.localStorage.getItem('id'))
     members = members.map((member, i) => 
       <div key={i} className="member">
         <Profile src={member.photo} />
